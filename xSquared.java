3e08f0244d013587150cd97c6cf44e5b5e399a9c
QuinnTange <118770255+QuinnTange@users.noreply.github.com>
2024-05-03 22:04:34 -0500

    cleanup

--- xSquared.java
+++ xSquared.java
@@ -1,16 +1,21 @@
-import java.util.List;
-import java.util.ArrayList;
+// import java.util.List;
+// import java.util.ArrayList;
 
-public class xSquared {
-    public static double xsquared(int numRuns, int[] B){
-        int[] C = new int[B.length];
-        for(int i = 0; i < C.length; i++){
-            C[i] = numRuns / B.length;
-        }
-        double x2 = 0;
-        for(int k = 0; k < C.length; k++){
-            x2+=((B[k]-C[k])^2)/(double)C[k];
-        }   
-        return x2;
-    }
-}
+
+/*
+ * Attempting to create code for statistical analysis (the x squared test), but was unable to complete code in time.
+ * Will utilize when we update code in our free time
+ */
+// public class xSquared {
+//     public static double xsquared(int numRuns, int[] B){
+//         int[] C = new int[B.length];
+//         for(int i = 0; i < C.length; i++){
+//             C[i] = numRuns / B.length;
+//         }
+//         double x2 = 0;
+//         for(int k = 0; k < C.length; k++){
+//             x2+=((B[k]-C[k])^2)/(double)C[k];
+//         }   
+//         return x2;
+//     }
+// }
